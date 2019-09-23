b74a5183d62f6528e0b90c679a0302fa4dbbeb9f
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2019-09-23 16:42:56 +0300

    Add startGame and render functions

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const hangman1 = new Hangman('Car car', 2);
-
 const puzzleDisplay = document.querySelector('#display-puzzle');
 const guessDisplay = document.querySelector('#display-guess');
 const statusEl = document.querySelector('#status');
 const ststusMsg = document.querySelector('#ststus-msg');
+const reset = document.querySelector('#reset');
+let hangman1;
 
-guessDisplay.textContent = hangman1.remainingGuesses;
-puzzleDisplay.textContent = hangman1.puzzle;
-console.log(hangman1.status)
+// guessDisplay.textContent = hangman1.remainingGuesses;
+// puzzleDisplay.textContent = hangman1.puzzle;
+// console.log(hangman1.status)
 
 window.addEventListener('keypress', e => {
     const guess = String.fromCharCode(e.charCode);
@@ -18,6 +18,20 @@ window.addEventListener('keypress', e => {
     statusEl.textContent = hangman1.status;
     ststusMsg.textContent = hangman1.statusMessage;
 })
+
+const render = () => {
+    guessDisplay.textContent = hangman1.remainingGuesses;
+    puzzleDisplay.textContent = hangman1.puzzle;
+}
+
+const startGame = async () => {
+    const puzzle = await getPuzzle('1');
+    hangman1 = new Hangman(puzzle, 3);
+    render();
+}
+
+reset.addEventListener('click', startGame);
+startGame();
 
 // const puzzle = getPuzzle('1', (err, puzzle) => {
 //     if(err) {
@@ -35,9 +49,9 @@ window.addEventListener('keypress', e => {
 //     console.log("Error :", err);
 // })
 
-const puzzle = getPuzzle('1').then(puzzle => {
-    console.log(puzzle);
-}).catch(err => {
-    console.log("Error :", err);
-});
+// const puzzle = getPuzzle('1').then(puzzle => {
+//     console.log(puzzle);
+// }).catch(err => {
+//     console.log("Error :", err);
+// });
 
