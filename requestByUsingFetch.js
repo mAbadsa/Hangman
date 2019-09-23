c8f23e85075572fcc3ed7e4a0dfc8085a53a7110
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2019-09-23 15:50:11 +0300

    Add reset button

--- requestByUsingFetch.js
+++ requestByUsingFetch.js
@@ -1,11 +1,21 @@
-const getPuzzle = (wordCount) => {
+const getPuzzle = async (wordCount) => {
+    const response = await fetch('http://puzzle.mead.io/puzzle?wordCount=' + wordCount, {});
+    if (response.status === 200) {
+        const data = await response.json();
+        return data;
+    } else {
+        throw new Error('Unable to fetch the puzzle!');
+    }
+}
+
+const getPuzzleOld = (wordCount) => {
     return fetch('http://puzzle.mead.io/puzzle?wordCount=' + wordCount, {
         headers: {
             'Content-Type': 'application/json'
         },
         method: 'GET'
     }).then(res => {
-        if(res.status === 200) {
+        if (res.status === 200) {
             return res.json();
         } else {
             throw new Error('Unable to fetch the puzzle!');
